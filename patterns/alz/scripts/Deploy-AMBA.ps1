7981f93561fd47a16a3318d9619195c64e84a196
Dmitriy Nekrasov <dnekrasov@microsoft.com>
2024-09-25 12:34:10 +0200

    Update alzArm.param.json and Deploy-AMBA.ps1
    Update values in alzArm.param.json for the enterprise scale company prefix, platform management group, identity management group, management management group, connectivity management group, landing zone management group, management subscription ID, and ALZ monitor resource group location. Also, add an email to the ALZ monitor action group.
    
    Add Deploy-AMBA.ps1 script to deploy AMBA (Azure Monitor Baseline Alerts) to the alz-demo1 management group in the swedencentral location.

---
+++ patterns/alz/scripts/Deploy-AMBA.ps1
@@ -0,0 +1,6 @@
+﻿$pseudoRootManagementGroup = "alz-demo1"
+$location = "swedencentral"
+
+New-AzManagementGroupDeployment -Name "amba-GeneralDeployment" -ManagementGroupId $pseudoRootManagementGroup -Location $location `
+  -TemplateUri "https://github.com/nekdima/azure-monitor-baseline-alerts-demo1/blob/main/patterns/alz/alzArm.json" `
+  -TemplateParameterFile ".\patterns\alz\alzArm.param.json"
