adc12641b9348f2007fb8b64cb21b167eab98cb1
Dmitriy Nekrasov <dnekrasov@microsoft.com>
2024-09-25 12:38:54 +0200

    Update AMBA deployment script with correct template URL and add verbose flag

--- patterns/alz/scripts/Deploy-AMBA.ps1
+++ patterns/alz/scripts/Deploy-AMBA.ps1
@@ -2,5 +2,6 @@
 $location = "swedencentral"
 
 New-AzManagementGroupDeployment -Name "amba-GeneralDeployment" -ManagementGroupId $pseudoRootManagementGroup -Location $location `
-  -TemplateUri "https://github.com/nekdima/azure-monitor-baseline-alerts-demo1/blob/main/patterns/alz/alzArm.json" `
-  -TemplateParameterFile ".\patterns\alz\alzArm.param.json"
+  -TemplateUri "https://raw.githubusercontent.com/nekdima/azure-monitor-baseline-alerts-demo1/main/patterns/alz/alzArm.json" `
+  -TemplateParameterFile ".\patterns\alz\alzArm.param.json" `
+  -Verbose
